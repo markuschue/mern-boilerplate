70721b4df7bcec3d09239d9f5c0f6882bf89cac5
Markus <alu0101130507@ull.edu.es>
2022-12-12 15:18:45 +0000

    boilerplate initialized

---
+++ client/src/components/App.jsx
@@ -0,0 +1,26 @@
+import logo from '../logo.svg';
+import './App.css';
+import Data from './Data.jsx';
+
+function App() {
+  const customFetchRoute = "http://localhost:9000/test/63973a30890b6a86a3f15b94";
+  /**
+   * The above route is a test route, and will return a JSON object with the given ID.
+   * You can also use the following routes:
+   *  - http://localhost:9000/test/ (returns all test objects)
+   *  - http://localhost:9000/      (returns a JSON object with the message "Hello World!")
+   * 
+   * For more details, see the 'api/routes/test.js' file.
+   */
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Data fetchRoute={customFetchRoute} />
+        <p>Fetched from <code>'{customFetchRoute}'</code></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
