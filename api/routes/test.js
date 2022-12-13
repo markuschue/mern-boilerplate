f2f4b1e53364d1ad6de78abfc24c88f74ec047ac
Markus <alu0101130507@ull.edu.es>
2022-12-13 16:58:51 +0000

    more restful api routing

--- api/routes/test.js
+++ api/routes/test.js
@@ -2,38 +2,83 @@ const express = require('express');
 const router = express.Router();
 const Test = require('../models/test.js');
 
-router.get('/', function(req, res, next) {
-  Test.find({}, function(err, tests) {
-    if (err)
-      return next(err);
-    else
-      res.json(tests);
-  });
-});
+router.route('/')
+  .get(function(req, res, next) {
+    Test.find(function(err, tests) {
+      if (err)
+        return next(err);
+      else
+        res.status(200).json(tests);
+    });
+  })
 
-router.get('/:id', function(req, res, next) {
-  Test.findById(req.params.id, function(err, post) {
-    if (err)
-      return next(err);
-    else if (post)
-      res.json(post);
-    else
-      res.status(404).send('Not found');
-  });
-});
+  .post(function(req, res, next) {
+    const test = new Test({
+      name: req.body.name,
+      age: req.body.age
+    });
+    test.save().then(() => {
+      res.status(200).send('Success uploading new test.');
+    });
+  })
 
-router.post('/', function(req, res, next) {
-  const test = new Test({
-    name: req.body.name,
-    age: req.body.age
+  .delete(function(req, res, next) {
+    Test.deleteMany({}, function (err) {
+      if (err)
+        return next(err);
+      else 
+        res.status(200).send('Success deleting all tests.');
+    });
   });
-  test.save().then(() => {
-    res.redirect('/test/');
-  });
-});
 
-/** 
- *  PUT, PATCH and DELETE not implemented, as this is a test model
- *  **/
+router.route('/:id')
+  .get(function(req, res, next) {
+    Test.findById(req.params.id, function(err, test) {
+      if (err)
+        return next(err);
+      else if (test)
+        res.status(200).json(test);
+      else
+        res.status(404).send('Test not found');
+    });
+  })
+
+  .put(function(req, res) {
+    Test.replaceOne(
+      {_id: req.params.testID},
+      {$set: req.body},
+      function (err) {
+        if (err)
+          next(err);
+        else
+          res.status(200).send('Test replaced successfully.');
+        res.status(404).send('No test found');
+      }
+    );
+  })
+
+  .patch(function(req, res) {
+    Test.updateOne(
+      {_id: req.params.testID},
+      req.body.patch,
+      function (err) {
+        if (err)
+          next(err);
+        else
+          res.status(200).send('Test patched successfully.');
+        res.status(404).send('No test found');
+      }
+    );
+  })
+
+  .delete(function(req, res) {
+    Test.deleteOne({_id: req.params.testID}, function (err) {
+      if (err)
+        next(err);
+      else
+        res.status(200).send('Test deleted successfully.');
+      res.status(404).send('No test found');
+    });
+  });
 
 module.exports = router;
