86b5dccbd9fbab7af0428003529d91b60df3b2bd
Markus <alu0101130507@ull.edu.es>
2022-12-13 16:15:28 +0000

    view engine leftover code removed

--- api/app.js
+++ api/app.js
@@ -14,10 +14,6 @@ const allowedOrigins = ['http://localhost:3000'/**,
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
